a4920f40253f098cb90b877a68ca1ea464158092
Chort Sereivathana <chortsereivathana@gmail.com>
2024-09-21 10:41:55 +0700

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './Component/Navbar/Navbar.jsx'
 import Hero from './Component/Hero/Hero.jsx'
 import Service from './Component/Service/Service.jsx'
 import Banner from './Component/Banner/Banner.jsx'
+import Subscribe from './Component/Subscribe/Subscribe.jsx'
+import Banner2 from './Component/Banner/Banner2.jsx'
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
       <Hero />
       <Service />
       <Banner />
+      <Subscribe />
+      <Banner2 />
     </main>
   )
 }
